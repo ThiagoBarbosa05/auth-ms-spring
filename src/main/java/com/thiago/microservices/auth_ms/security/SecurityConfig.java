94160400dcf53dab1bde31d8c785846175372b32
thiago barbosa <thiagon16@hotmail.com>
2025-03-07 23:08:45 -0300

    feat: permissions swagger endpoint and create user endpoint

--- src/main/java/com/thiago/microservices/auth_ms/security/SecurityConfig.java
+++ src/main/java/com/thiago/microservices/auth_ms/security/SecurityConfig.java
@@ -2,13 +2,36 @@ package com.thiago.microservices.auth_ms.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private static final String[] SWAGGER_LIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resource/**",
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                                .requestMatchers(SWAGGER_LIST).permitAll()
+                                .anyRequest().authenticated()
+                        );
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
